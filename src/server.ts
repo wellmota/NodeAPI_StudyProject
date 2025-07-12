223b881678d4317ed0c14e571566ca4c4c4744bd
wellmota <impeto.wellington@gmail.com>
2025-07-12 15:08:42 -0300

    Add zod dependency to package.json and package-lock.json, refactor database and server files to use env for configuration

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Test Transaction',
-      amount: 5000,
-    })
-    .returning('*')
+app.register(transactionsRoutes)
 
-  return transaction
-})
-
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server is running on port 3333')
 })
