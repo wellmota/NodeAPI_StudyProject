b92d0f635db1957b09d69acfcb96bec346f3592c
wellmota <impeto.wellington@gmail.com>
2025-07-12 13:39:45 -0300

    Refactor /hello endpoint to insert a new transaction into the database using Knex

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,15 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Test Transaction',
+      amount: 5000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => {
