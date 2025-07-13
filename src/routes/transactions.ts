a8a3cb6ec5a65f262044053b9a2d42cd4f3547bc
wellmota <impeto.wellington@gmail.com>
2025-07-13 12:14:26 -0300

    Add summary endpoint to transactions routes for retrieving total transaction amounts

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -10,13 +10,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
   app.get('/:id', async (request) => {
     const getTransactionParamsSchema = z.object({ id: z.string().uuid() })
-    
-    const {id} = getTransactionParamsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where('id',id).first()
+    const { id } = getTransactionParamsSchema.parse(request.params)
 
-    return {transaction}
+    const transaction = await knex('transactions').where('id', id).first()
 
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+
+    return { summary }
   })
 
   app.post('/', async (request, reply) => {
