0bfdf909f96d15464b7fbd19efeade5ac5e04bea
wellmota <impeto.wellington@gmail.com>
2025-07-13 12:29:27 -0300

    Update session cookie maxAge in transactions routes to 7 days in seconds

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -47,7 +47,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
       reply.cookie('sessionId', sessionId, {
         path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
+        maxAge: 60 * 60 * 24 * 7, //7 days
       })
     }
 
