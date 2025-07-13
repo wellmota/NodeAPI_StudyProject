762d2e730c643a6fb2f220848a068d8f3268ac69
wellmota <impeto.wellington@gmail.com>
2025-07-13 12:28:31 -0300

    Add @fastify/cookie dependency and implement session management in transactions routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto'
 import { FastifyInstance } from 'fastify'
 import z from 'zod'
 import { knex } from '../database'
@@ -39,10 +40,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, //7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
