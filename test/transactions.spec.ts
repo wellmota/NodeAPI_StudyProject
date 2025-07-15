95756330c295fd993754fa07e34a3d4a9027f142
wellmota <impeto.wellington@gmail.com>
2025-07-14 23:43:52 -0300

    Enhance transaction tests by adding cases for retrieving a specific transaction and getting a summary of transactions. Clean up test setup by removing unnecessary imports and whitespace.

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -3,9 +3,7 @@ import { randomUUID } from 'crypto'
 process.env.DATABASE_URL = process.env.DATABASE_URL ?? './test.db'
 process.env.NODE_ENV = 'test'
 
-import { execSync } from 'node:child_process'
-
-import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import request from 'supertest'
 import { app } from '../src/app'
@@ -13,7 +11,6 @@ import { knex } from '../src/database'
 
 describe('transactions routes', () => {
   beforeAll(async () => {
-    
     // Run all pending migrations before the application is ready
     await knex.migrate.latest()
     await app.ready()
@@ -24,7 +21,6 @@ describe('transactions routes', () => {
     // Destroy the database connection after tests finish
     await knex.destroy()
   })
-
 
   it('should be able to create a new transaction', async () => {
     const sessionId = randomUUID()
@@ -65,5 +61,72 @@ describe('transactions routes', () => {
         amount: 5000,
       }),
     ])
+  })
+
+  it('should to get a specific transaction', async () => {
+    const sessionId = randomUUID()
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .set('Cookie', [`sessionId=${sessionId}`])
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') as string[]
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionsResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionsResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      })
+    )
+  })
+
+  it('should be able to get the summary', async () => {
+    const sessionId = randomUUID()
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .set('Cookie', [`sessionId=${sessionId}`])
+      .send({
+        title: 'Credit Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') as string[]
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', [`sessionId=${sessionId}`])
+      .send({
+        title: 'Debit Transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
   })
 })
