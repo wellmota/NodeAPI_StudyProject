d740011b237803859abb9de93b0620392337d526
wellmota <impeto.wellington@gmail.com>
2025-07-14 22:53:39 -0300

    Refactor testing setup by replacing Jest with Vitest in package.json and tsconfig.json. Update transactions route to simplify session handling and improve code readability. Remove outdated example.spec.ts file.

---
+++ test/transactions.spec.ts
@@ -0,0 +1,38 @@
+import { randomUUID } from 'crypto'
+// Set required environment variables for the test **before** importing the application code
+process.env.DATABASE_URL = process.env.DATABASE_URL ?? './test.db'
+process.env.NODE_ENV = 'test'
+
+import { afterAll, beforeAll, describe, it } from 'vitest'
+
+import request from 'supertest'
+import { app } from '../src/app'
+import { knex } from '../src/database'
+
+describe('transactions routes', () => {
+  beforeAll(async () => {
+    // Run all pending migrations before the application is ready
+    await knex.migrate.latest()
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+    // Destroy the database connection after tests finish
+    await knex.destroy()
+  })
+
+  it('should be abel to create a new transaction', async () => {
+    const sessionId = randomUUID()
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', [`sessionId=${sessionId}`])
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
